e2632d6c4406be6790d84be78cc7973bdb465677
Will Crichton <willcrichton@cmu.edu>
2014-03-12 22:42:07 -0500

    Added effects to action buttons

--- static/js/views/ActionsView.js
+++ static/js/views/ActionsView.js
@@ -17,7 +17,26 @@ define(function(require) {
 
         doAction: function(e) {
             var action = $(e.target).data('action');
+            var duration = 0;
+            GAME.actions.forEach(function(a) {
+                if (a.get('name') == action) {
+                    duration = a.get('duration');
+                }
+            });
+
             GAME.doAction(action);
+
+            var time = new Date().getTime();
+            var timer = setInterval(function() {
+                var diff = (new Date().getTime() - time) / 1000;
+                var percent = diff / duration * 100;
+                $(e.target).css('background', 'linear-gradient(to right, rgba(0, 0, 0, 0.1) ' + percent + '%, transparent ' + percent + '%)')
+            }, 10);
+
+            setTimeout(function() {
+                clearInterval(timer);
+                $(e.target).css('background', 'transparent');
+            }, duration * 1000);
         },
 
         render: function() {
