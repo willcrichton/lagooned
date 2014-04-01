7707aab0c4d6c0b369790e5b57c9fc1bcf4d77f4
Ivan Wang <icw@cmu.edu>
2014-03-31 20:45:47 -0400

    added tier 1 actions and items

--- constants.py
+++ constants.py
@@ -8,8 +8,13 @@ C = {
     # Locations
     'LOCATION_START': 'empty beach',
     'LOCATION_FOREST': 'dense rainforest',
+    'LOCATION_RUINS': 'scattered ruins',
 
     # Actions
+    'ACT_MOVE_FOREST': 'You walk toward the trees and find yourself in a dense rainforest. The sound of running water flows from afar.',
+    'ACT_MOVE_RUINS': 'You find yourself in the scattered remains of an ancient building. Bricks and rotted wood lie buried in the sand.',
+    'ACT_MOVE_START': 'You return to the beach and the ebbing shore.',
+
     'ACT_SCAVENGE': 'Scavenge',
     'ACT_SCAVENGE_SUCCESS': 'You found some food lying around.',
     'ACT_SCAVENGE_FAIL': 'You failed to find any food.',
@@ -17,10 +22,55 @@ C = {
     'ACT_COOK': 'Cook',
     'ACT_COOK_SUCCESS': 'You cooked some food over a roaring fire.',
 
-    'ACT_FIREWOOD': 'Find firewood',
-    'ACT_FIREWOOD_SUCCESS': 'You found some firewood.',
+    'ACT_FIREWOOD': 'Gather wood',
+    'ACT_FIREWOOD_SUCCESS_TWIGS': 'You gathered some twigs from the ground.',
+    'ACT_FIREWOOD_SUCCESS_MOSS': 'You collected some dry moss growing on a trunk.',
+    'ACT_FIREWOOD_SUCCESS_BRANCHES': 'You broke off a branch from a small tree.',
     'ACT_FIREWOOD_FAIL': 'You couldn\'t find any firewood.',
 
+    'ACT_FORAGE': 'Forage for food',
+    'ACT_FORAGE_SUCCESS_COCONUTS': 'You shook a tree, and a coconut fell to your feet.',
+    'ACT_FORAGE_SUCCESS_BERRIES': 'You discovered some berries in a bush.',
+    'ACT_FORAGE_SUCCESS_SEA_GRASS': 'You yanked some slimy grass from the seabed.'
+    'ACT_FORAGE_SUCCESS_FLOWERS': 'You picked a bunch of sweet-smelling flowers.',
+    'ACT_FORAGE_SUCCESS_CLAM': 'You found a clam lying on the seabed.',
+    'ACT_FORAGE_FAIL': 'You failed to find any food.',
+
+    'ACT_HUNT_CRABS': 'Hunt crabs',
+    'ACT_HUNT_CRABS_SUCCESS': 'You trapped and slew a crab.',
+    'ACT_HUNT_CRABS_FAIL': 'You attempt to grab a crab, but it snaps at you and scurries under a rock.',
+
+    'ACT_HUNT_SHEEP': 'Hunt sheep',
+    'ACT_HUNT_SHEEP_SUCCESS': 'You gut a sheep as its dying baa rings in your ears.',
+    'ACT_HUNT_SHEEP_FAIL': 'You try to tackle a sheep, but it baas and kicks off instead.',
+
+    # Random Events
+    'RANDOM_WASHUP_SAIL': 'You notice a sail floating on the horizon.',
+    'RANDOM_WASHUP_ROPE': 'A small rope floats amidst the flotsam.',
+    'RANDOM_WASHUP_BOTTLE': 'You picked up a bottle drifting in the water. A small crumpled paper is sealed inside.',
+    'RANDOM_WASHUP_DRIFTWOOD': 'You unearthed a piece of driftwood lying in the sand.',
+
+    'RANDOM_TREASURE_GOLD': 'A glimmer catches your eye, and you discover an ingot of gold under a broken stone.',
+
     # Items
-    'ITEM_FIREWOOD': 'Firewood'
+    'ITEM_FIREWOOD': 'Firewood',
+    'ITEM_DRIFTWOOD': 'Driftwood',
+    'ITEM_TWIGS': 'Twigs',
+    'ITEM_MOSS': 'Dry Moss',
+    'ITEM_BRANCHES': 'Branches',
+
+    'ITEM_COCONUT': 'Coconut',
+    'ITEM_BERRIES': 'Berries',
+    'ITEM_SEA_GRASS': 'Sea Grass',
+    'ITEM_FLOWERS': 'Flowers'
+    'ITEM_CLAM': 'Clam',
+
+    'ITEM_CRAB': 'Crab',
+    'ITEM_SHEEP': 'Sheep',
+
+    'ITEM_SAIL': 'Sail',
+    'ITEM_ROPE': 'Rope',
+    'ITEM_BOTTLE': 'Message-in-a-bottle',
+    'ITEM_GOLD': 'Gold Nugget'
+
 }
