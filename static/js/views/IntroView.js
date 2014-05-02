ec48e54dbc5b7d114770546faedacbc8aafd9f2b
Will Crichton <willcrichton@cmu.edu>
2014-05-02 16:35:18 -0400

    EMERGENCY FIX

--- static/js/views/IntroView.js
+++ static/js/views/IntroView.js
@@ -7,7 +7,7 @@ define(function(require) {
         render: function() {
             setTimeout(_.bind(function() {
                 this.router.navigate('home', {trigger: true});
-            }, this), 0);
+            }, this), 9000);
 
             this.$el.html(template);
             return this;
